463a2ce7ab6f8fbb2f98151d4bde4ed0df47324d
papercdisc <kflores3@inside.artcenter.edu>
2024-09-09 09:49:25 -0700

    IntellisenseTest script added

---
+++ EGAM202 Week1 Test/Assets/Scripts/IntellisenseTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntellisenseTest : MonoBehaviour
+{
+    public GameObject kayFlores;
+    // Start is called before the first frame update
+    void Start()
+    {
+        kayFlores.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
