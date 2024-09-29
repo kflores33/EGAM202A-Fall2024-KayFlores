3af37955451e98b58c35c6faa8b631302f4c847c
papercdisc <kflores3@inside.artcenter.edu>
2024-09-28 17:38:04 -0700

    a few new scripts and psuedocode

--- Cover1Pikmin/Assets/Scripts/MoveableCamera.cs
+++ Cover1Pikmin/Assets/Scripts/MoveableCamera.cs
@@ -15,4 +15,15 @@ public class MoveableCamera : MonoBehaviour
     {
         
     }
+
+    // get mouse cursor position
+
+    // if cursor raycast comes in contact with the edge sections of the screen, move the camera towards that direction
+    // (change x and z, not y)
+    // i think like.... maybe moving the cursor changes the position of the camera's look at point? prolly wouldnt work though..
+    // wait no im stupid just have the reference transform in an empty game object that moves with the cursor. wait. but then how would...the fuckin..
+    // how would the boundary thing work? wait. have it in an if statement? like. only have the transform follow the cursor when its out of bounds. or sumn.
+
+    // to create the boundary, i could prolly put actual gameobjects the cursor can collide with and have them parented to the camera (and have
+    // them be invisible ofc
 }
